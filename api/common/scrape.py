bc9b7064d06aa9da3ae01e4c7ba64c265bfb989d
Nazrawi Demeke <dnazrawi@gmail.com>
2022-12-01 08:55:37 +0300

    add team and league ids

--- api/common/scrape.py
+++ api/common/scrape.py
@@ -2,7 +2,7 @@ import bs4
 import requests
 from api.common.constants import *
 
-def scrape(url):
+def scrape(url, league):
     html = requests.get(url).text
     soup = bs4.BeautifulSoup(html, 'lxml')
 
@@ -10,13 +10,16 @@ def scrape(url):
 
     table = []
     for row in data:
+        name = (row.find('span', class_='widget-match-standings__team--full-name')).text.strip()
         team = {
+            'id': str(abs(hash(name))),
             'rank': int((row.find('td')).text.strip()),
-            'name': (row.find('span', class_='widget-match-standings__team--full-name')).text.strip(),
+            'name': name,
             'points': int((row.find('td', class_='widget-match-standings__pts')).text.strip()),
             'goals': (row.find('td', class_='widget-match-standings__goals-diff')).text.strip(),
             'matches': int((row.find('td', class_='widget-match-standings__matches-played')).text.strip()),
-            'logo': row.find('img', class_='widget-match-standings__crest')['src'].strip()
+            'logo': row.find('img', class_='widget-match-standings__crest')['src'].strip(),
+            'leagueId': league
         }
         table.append(team)
 
