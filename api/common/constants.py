f97ef967aa35574112a25ef1559b67c4dd8c571c
Nazrawi Demeke <dnazrawi@gmail.com>
2022-12-01 11:23:28 +0300

    change source website

--- api/common/constants.py
+++ api/common/constants.py
@@ -1,22 +1,6 @@
 class Url:
-    PREMIER_LEAGUE = 'https://www.goal.com/en/premier-league/table/2kwbbcootiqqgmrzs6o5inle5'
-    CHAMPIONSHIP = 'https://www.goal.com/en-gb/championship/table/7ntvbsyq31jnzoqoa8850b9b8'
-    LA_LIGA = 'https://www.goal.com/en-ug/primera-divisi%C3%B3n/table/34pl8szyvrbwcmfkuocjm3r6t'
-    BUNDESLIGA = 'https://www.goal.com/en-ug/bundesliga/table/6by3h89i2eykc341oz7lv1ddd'
-    LEAGUE_1 = 'https://www.goal.com/en/ligue-1/table/dm5ka0os1e3dxcp3vh05kmp33'
-
-class Site:
-    CLASSES = (
-        'widget-match-standings__row widget-match-standings__row-- widget-match-standings__row--rank-status widget-match-standings__row--rank-status-1',
-        'widget-match-standings__row widget-match-standings__row--active widget-match-standings__row--rank-status widget-match-standings__row--rank-status-1',
-        'widget-match-standings__row widget-match-standings__row-- widget-match-standings__row--rank-status widget-match-standings__row--rank-status-2',
-        'widget-match-standings__row widget-match-standings__row--active widget-match-standings__row--rank-status widget-match-standings__row--rank-status-2',
-        'widget-match-standings__row widget-match-standings__row-- widget-match-standings__row--rank-status widget-match-standings__row--rank-status-3',
-        'widget-match-standings__row widget-match-standings__row--active widget-match-standings__row--rank-status widget-match-standings__row--rank-status-3',
-        'widget-match-standings__row widget-match-standings__row-- widget-match-standings__row--rank-status widget-match-standings__row--rank-status-4',
-        'widget-match-standings__row widget-match-standings__row--active widget-match-standings__row--rank-status widget-match-standings__row--rank-status-4',
-        'widget-match-standings__row widget-match-standings__row-- widget-match-standings__row--rank-status widget-match-standings__row--rank-status-',
-        'widget-match-standings__row widget-match-standings__row--active widget-match-standings__row--rank-status widget-match-standings__row--rank-status-',
-        'widget-match-standings__row widget-match-standings__row-- widget-match-standings__row--rank-status widget-match-standings__row--rank-status-relegation',
-        'widget-match-standings__row widget-match-standings__row--active widget-match-standings__row--rank-status widget-match-standings__row--rank-status-relegation'
-    )
+    PREMIER_LEAGUE = 'https://www.theguardian.com/football/premierleague/table'
+    CHAMPIONSHIP = 'https://www.theguardian.com/football/championship/table'
+    LA_LIGA = 'https://www.theguardian.com/football/laligafootball/table'
+    BUNDESLIGA = 'https://www.theguardian.com/football/bundesligafootball/table'
+    LEAGUE_1 = 'https://www.theguardian.com/football/ligue1football/table'
